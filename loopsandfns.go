4ae1041d23b8c221328ab0fd2e878f02d1f63985
oodabaga <oodabaga@gmail.com>
2013-09-05 10:39:26 -0400

    First exercise complete.
    Calculate the square root of a number and compare it to the math
    package's value.

---
+++ loopsandfns.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const delta = 1e-10
+
+func Sqrt(x float64) float64 {
+	z := x
+	for {
+		n := z - (z*z-x)/(2*z)
+		if math.Abs(n-z) < delta {
+			break
+		}
+		z = n
+	}
+	return z
+}
+
+func main() {
+  vals := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+  for _, v := range vals {
+    fmt.Printf("%f\t%f\n", Sqrt(float64(v)), math.Sqrt(float64(v)))
+  }
+}
